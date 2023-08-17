f8ba33e5319f3f7a94494945754125f3dc65878c
cesur bağcı <cesurbagci@gmail.com>
2023-08-17 22:52:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const firebaseRemoteConfigLib     = require('./lib/firebaseRemoteConfigLib');
+
+exports.firebaseRemoteConfig = { };
+exports.firebaseRemoteConfig.initializeApp = firebaseRemoteConfigLib.initializeApp;
+exports.firebaseRemoteConfig.validateConfig = firebaseRemoteConfigLib.validateConfig;
+exports.firebaseRemoteConfig.printConfigInRemote = firebaseRemoteConfigLib.printConfigInRemote;
+exports.firebaseRemoteConfig.pullConfigMeta = firebaseRemoteConfigLib.pullConfigMeta;
+exports.firebaseRemoteConfig.pushConfig = firebaseRemoteConfigLib.pushConfig;
+exports.firebaseRemoteConfig.pullConfig = firebaseRemoteConfigLib.pullConfig;
