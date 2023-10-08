fe0716e1f880b1c4c06b083d41bff4b76a4ecfa2
cesur bağcı <cesurbagci@gmail.com>
2023-10-08 23:12:40 +0300

    getCurrentVersionInfo is added.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const firebaseRemoteConfigLib     = require('./lib/firebaseRemoteConfigLib');
+const firebaseRemoteConfigLib = require('./lib/firebaseRemoteConfigLib');
 
-exports.firebaseRemoteConfig = { };
+exports.firebaseRemoteConfig = {};
 exports.firebaseRemoteConfig.initializeApp = firebaseRemoteConfigLib.initializeApp;
+exports.firebaseRemoteConfig.getCurrentVersionInfo = firebaseRemoteConfigLib.getCurrentVersionInfo;
 exports.firebaseRemoteConfig.increaseVersion = firebaseRemoteConfigLib.increaseVersion;
 exports.firebaseRemoteConfig.validateConfig = firebaseRemoteConfigLib.validateConfig;
 exports.firebaseRemoteConfig.printConfigInRemote = firebaseRemoteConfigLib.printConfigInRemote;
